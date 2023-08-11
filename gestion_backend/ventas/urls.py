46303942fa1793914f0548c597e212004f017f79
zLeaff <julii.balinio@gmail.com>
2023-08-11 15:47:49 -0300

    fix: ventas urls

--- gestion_backend/ventas/urls.py
+++ gestion_backend/ventas/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import OrderViewset, OrderCreateView
+from .views import OrderViewset
 
 urlpatterns = [
-    path('orders/', OrderViewset.as_view({'get': 'list'}), name='order-list'),
+    path('orders/', OrderViewset.as_view({'get': 'list', 'post': 'create'}), name='order-list'),
     path('orders/<int:pk>/', OrderViewset.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'delete'}), name='order-detail'),
-    path('orders/create/', OrderCreateView.as_view(), name='order-create'),
 ]
